44d6d4614f662b55a59c62e5de7cb040c98f2782
xelaushi <xelaushi@posteo.net>
2021-04-24 17:35:18 -0400

    Remove uses of checkerframework - didn't use it enough

--- src/main/java/cs340/getpet/persistence/SearchQuery.java
+++ src/main/java/cs340/getpet/persistence/SearchQuery.java
@@ -1,7 +1,5 @@
 package cs340.getpet.persistence;
 
-import checkers.nullness.quals.NonNull;
-import checkers.nullness.quals.Nullable;
 
 /**
  * A search query for finding an animal.
@@ -10,25 +8,25 @@ public class SearchQuery {
     /**
      * The species of animal to search for.
      */
-    public final @NonNull Species species;
+    public final Species species;
     /**
      * The genders of animal to search for.
      */
-    public final @Nullable Gender[] genders;
+    public final Gender[] genders;
     /**
      * The breed of animal to search for.
      */
-    public final @Nullable String breed;
+    public final String breed;
     /**
      * The colors of animal to search for.
      */
-    public final @Nullable Color[] colors;
+    public final Color[] colors;
     /**
      * The sizes of animal to search for.
      */
-    public final @Nullable Size[] sizes;
+    public final Size[] sizes;
 
-    protected SearchQuery(@NonNull Species species, @Nullable Gender[] genders, @Nullable String breed, @Nullable Color[] colors, @Nullable Size[] sizes) {
+    protected SearchQuery(Species species, Gender[] genders, String breed, Color[] colors, Size[] sizes) {
         this.species = species;
         this.genders = genders;
         this.breed = breed;
